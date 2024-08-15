739a33550af55772b0cdcab923a9dec67dd35e1d
eranmes <1072163+eranmes@users.noreply.github.com>
2024-08-15 10:49:26 +0200

    sm6375-common: Copy out permission for target that supports Device ID
     * https://source.android.com/security/keystore/attestation
    
    Change-Id: Ibeebb76e45338c24d41d82b680c3353af3ca478f

--- common.mk
+++ common.mk
@@ -120,6 +120,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Device ID attestation
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
